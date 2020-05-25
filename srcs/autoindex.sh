3c8182fb9f0c634a844e24ded479abace48b6348
OphelieBari <53915679+OphelieBari@users.noreply.github.com>
2020-05-25 16:48:14 +0200

    Delete autoindex.sh

--- srcs/autoindex.sh
+++
@@ -1,4 +0,0 @@
-mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default3
-mv /etc/nginx/sites-available/default2 /etc/nginx/sites-available/default
-mv /etc/nginx/sites-available/default3 /etc/nginx/sites-available/default2
-service nginx restart
